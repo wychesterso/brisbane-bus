9a3aee694988de52f293029150fb5cfe93471dbe
wychesterso <chesterwyso@gmail.com>
2026-01-02 10:49:48 +0800

    initial commit

---
+++ src/test/java/com/wychesterso/transit/brisbane_bus/BrisbaneBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wychesterso.transit.brisbane_bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrisbaneBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
